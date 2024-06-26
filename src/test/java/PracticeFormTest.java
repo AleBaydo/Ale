1955d06200a23556697a2c505913da66a97308c7
Alejandro <167961563+AleBaydo@users.noreply.github.com>
2024-06-26 20:38:45 +0600

    Update PracticeFormTest.java

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -5,7 +5,13 @@ import org.testng.annotations.Test;
 import static com.demoqas.enums.Endpoints.*;
 
 public class PracticeFormTest extends BaseTest {
-    @Test
+    @Test(groups = {"Regression", "UI", "0405"}, description = "Verify double click button is working propertl")
+    @Owner("Alejandro")
+    @Tag("Regression")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("GCPINT-0405")
+    @Epic("Form")
+    @Link("www.demoqa.com")
     public void testFormFill() throws InterruptedException {
         browserHelper.open(ConfigReader.getValue("baseURL")+ PRACTICE.getEndpoint());
         PracticeFormEntity practiceFormEntity = randomUtils.generateRandomPracticeFormEntity();
