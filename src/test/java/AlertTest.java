f93b3c0004f281a244e4bbcd8793d0b851d91e41
Alejandro <167961563+AleBaydo@users.noreply.github.com>
2024-06-26 21:15:50 +0600

    Update AlertTest.java

--- src/test/java/AlertTest.java
+++ src/test/java/AlertTest.java
@@ -4,7 +4,13 @@ import org.testng.annotations.Test;
 
 public class AlertTest extends BaseTest {
 
-    @Test
+     @Test(groups = {"E2E", "UI", "1011"}, description = "Alert test")
+    @Owner("Zhyldyz")
+    @Tag("E2E")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("1011")
+    @Epic("Alerts")
+    @Link("www.demoqa.com")
     void aLertTest() throws InterruptedException {
         driver.get("https://demoqa.com/alerts");
         webElementActions.click(demoQAPages.getAlertPage().confirmAlertBtn);
