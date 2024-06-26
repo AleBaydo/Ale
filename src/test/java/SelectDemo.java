d256c91126691e9c7d899c85f56bc735af121501
Alejandro <167961563+AleBaydo@users.noreply.github.com>
2024-06-26 20:36:49 +0600

    Update SelectDemo.java

--- src/test/java/SelectDemo.java
+++ src/test/java/SelectDemo.java
@@ -8,7 +8,14 @@ import static com.demoqas.enums.Endpoints.ALERT;
 import static com.demoqas.enums.Endpoints.DEMO;
 
 public class SelectDemo extends BaseTest{
-  @Test
+   @Test(groups = {"E2E", "UI", "0404"}, description = "Verify double click button is working propertl")
+    @Owner("Alejandro")
+    @Tag("E2E")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("GCPINT-0404")
+    @Epic("Select")
+    @Link("www.demoqa.com")
+
   void test123() throws InterruptedException {
       browserHelper.open(ConfigReader.getValue("baseURL")+ DEMO.getEndpoint());
       WebElement selectMenu = driver.findElement(By.id("oldSelectMenu"));
